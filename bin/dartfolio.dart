2c670d2f360a96bd9ed741a62dfbeed4a77a3bbd
Alyssoncle <alyssoncpa@hotmail.com>
2022-09-08 21:18:34 -0300

    first commit

---
+++ bin/dartfolio.dart
@@ -0,0 +1,6 @@
+import 'package:dartfolio/dartfolio.dart' as dartfolio;
+
+///git@github.com:Alyssoncle/Dartfolio.git
+void main(List<String> arguments) {
+  print('Hello world: ${dartfolio.calculate()}!');
+}
